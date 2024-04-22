19565ab92161351efe9bf9999a7079cdc848c889
AkshaySatasiya <akshaysatasiya2811@gmail.com>
2024-04-22 23:41:34 +0530

    yolo inference

---
+++ yolo_inference.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+
+model = YOLO('yolov8x')
+
+results = model.predict("input_videos/08fd33_4.mp4", save=True)
+
+print(results[0])
+print("=============================")
+for box in results[0].boxes:
+    print(box)
